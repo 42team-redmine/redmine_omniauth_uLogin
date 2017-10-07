a87d7ec0b1600c91efffd83d00b7698585d7f1cc
xziy <cziy@ya.ru>
2017-10-07 17:18:28 +0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -3,15 +3,15 @@ require_dependency 'redmine_omniauth_ulogin/hooks'
 
 Redmine::Plugin.register :redmine_omniauth_ulogin do
   name 'Redmine Omniauth uLogin plugin'
-  author 'Dmitry Kovalenok, Alexey Marochkin'
-  description 'This is a plugin for Redmine registration through uLogin'
-  version '0.0.1'
-  url 'https://github.com/mrscylla/redmine_omniauth_uLogin'
-  author_url 'http://psiconsul.ru'
+  author 'Dmitry Kovalenok, Alexey Marochkin, Xziy Vash'
+  description 'This is a plugin for Redmine registration through uLogin for 3.X'
+  version '0.0.2'
+  url 'https://github.com/42team-redmine/redmine_omniauth_uLogin'
+  author_url 'http://xziy.ru'
 
   settings :default => {
     :client_id => "",
     :oauth_autentification => false,
     :allowed_domains => ""
-  }, :partial => 'settings/ulogin'
+  }, :partial => 'settings/ulogin_settings'
 end
